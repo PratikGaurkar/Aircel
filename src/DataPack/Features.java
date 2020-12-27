286a29f22aa385cebd2b0044d88b3004973ecb78
A <A@abc>
2020-12-26 22:24:59 -0800

    [vaibhav]:done the work

--- src/DataPack/Features.java
+++ src/DataPack/Features.java
@@ -4,6 +4,6 @@ public class Features {
 public static void main(String[] args) {
 	
 	System.out.println("Value Added Services....");
-	System.out.println("added services for particular scenarios ");
+	System.out.println("added services for particular scenarios");
 }
 }
